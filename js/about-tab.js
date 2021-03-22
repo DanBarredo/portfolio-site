c0c5cc8af5e324d41a05366b50f0c9ccacbf21de
DanBarredo <dvbarredo@gmail.com>
2021-03-22 12:39:15 +0000

    added some content

--- js/about-tab.js
+++ js/about-tab.js
@@ -3,16 +3,16 @@ const btns = document.querySelectorAll(".tab-btn");
 const aboutContents = document.querySelectorAll(".about-content");
 
 about.addEventListener("click", (e) => {
-  const id = e.target.dataset.id;
-  if (id) {
-    btns.forEach(function (btn) {
-      btn.classList.remove("active");
-      e.target.classList.add("active");
-    });
-    aboutContents.forEach((content) => {
-      content.classList.remove("active");
-    });
-    const elem = document.getElementById(id);
-    elem.classList.add("active");
-  }
+    const id = e.target.dataset.id;
+    if (id) {
+        btns.forEach(function (btn) {
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        aboutContents.forEach((content) => {
+            content.classList.remove("active");
+        });
+        const elem = document.getElementById(id);
+        elem.classList.add("active");
+    }
 });
